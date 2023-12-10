643932b79c3b31d5caf947bbed91adfdb87720f1
Pedro Henrique Guerra De Abreu <49170372+PedroHenriqueGuerra@users.noreply.github.com>
2023-12-10 19:42:09 -0300

    primeiro commit

--- src/main/java/com/example/projetoWeb/service/FilmeService.java
+++ src/main/java/com/example/projetoWeb/service/FilmeService.java
@@ -1,4 +1,24 @@
 package com.example.projetoWeb.service;
 
+import com.example.projetoWeb.model.Filme;
+import com.example.projetoWeb.repository.FilmeRepository;
+
+import java.util.List;
+
 public class FilmeService {
+
+    private FilmeRepository filmeRepository;
+
+//    public List<Filme> listarTodos(){
+//
+//    }
+//
+//    public List<Filme> listarPorGenero(String genero){
+//
+//    }
+//
+//    public Filme listarPorNome(String nome){
+//
+//    }
+
 }
